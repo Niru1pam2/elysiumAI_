bdfb6346de640f33e3e8ffc1b4f9cf05b668ab54
Niru1pam2 <nirupamtalukdar123@gmail.com>
2026-07-21 20:52:50 +0530

    fix: stop tracking node_modules and env files

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import router from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use("/", router);
 const PORT = process.env.PORT || 8001;
 
 app.get("/", (req, res) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,28 @@
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../utils/firebase";
+import api from "../utils/axios";
+
 function App() {
-  return <div>App</div>;
+  const handleLogin = async (token) => {
+    try {
+      const { data } = await api.post("/auth/login", { token });
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const googleLogin = async () => {
+    const data = await signInWithPopup(auth, googleProvider);
+    const token = await data.user.getIdToken();
+    await handleLogin(token);
+  };
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <button className=" bg-amber-400" onClick={googleLogin}>
+        continue with google
+      </button>
+    </div>
+  );
 }
 export default App;
